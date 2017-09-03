d46cd3c1beb075ebe1eecea631acbd4fa8821992
Hayato oyanagi <oyana@oya.local>
2017-09-03 17:16:01 +0900

    remote strpfd

---
+++ app/controllers/hoge_controller.rb
@@ -0,0 +1,34 @@
+class TermsController < ApplicationController
+  def new
+    Term.find(1)
+    @term = Term.new
+    @term.samples.build
+  end
+
+  def create
+    Term.find(1)
+    @term = Term.new(name: params[:name], description: params[:description])
+    if @term.save
+      redirect_to "terms/new"
+    else
+      render action => :new
+    end
+  end
+
+    def index
+      respond_to do |format|
+        format.html
+        format.json do
+          keyword = params[:keyword].gsub(/(\r\n|\r|\n)/, "")
+          @terms = Term.where("name like '%#{keyword}%'")
+          # @samples = []
+          # @terms.each do |term|
+          #   binding.pry
+          #   @samples << term.samples
+          # end
+        end
+      end
+    end
+
+  private
+end
